47c170eea8817e39d1339653cc3c4531552ef1a7
harshdadwal <harsh_dadwal@yahoo.com>
2019-03-22 15:09:31 -0400

    changes

--- freertos_blinky/example/src/freertos_blinky.c
+++ freertos_blinky/example/src/freertos_blinky.c
@@ -1,34 +1,19 @@
 /*
  * @brief FreeRTOS Blinky example
  *
- *Edited by Harsh Dadwal(731805) and Abhishek Parikhh(732182)
- *
  * @note
  * Copyright(C) NXP Semiconductors, 2014
  * All rights reserved.
  *
- * @par
- * Software that is described herein is for illustrative purposes only
- * which provides customers with programming information regarding the
- * LPC products.  This software is supplied "AS IS" without any warranties of
- * any kind, and NXP Semiconductors and its licensor disclaim any and
- * all warranties, express or implied, including all implied warranties of
- * merchantability, fitness for a particular purpose and non-infringement of
- * intellectual property rights.  NXP Semiconductors assumes no responsibility
- * or liability for the use of the software, conveys no license or rights under any
- * patent, copyright, mask work right, or any other intellectual property rights in
- * or to any products. NXP Semiconductors reserves the right to make changes
- * in the software without notification. NXP Semiconductors also makes no
- * representation or warranty that such application will be suitable for the
- * specified use without further testing or modification.
  *
- * @par
- * Permission to use, copy, modify, and distribute this software and its
- * documentation is hereby granted, under NXP Semiconductors' and its
- * licensor's relevant copyrights in the software, without fee, provided that it
- * is used in conjunction with NXP Semiconductors microcontrollers.  This
- * copyright, permission, and disclaimer notice must appear in all copies of
- * this code.
+ *
+ *
+ * Edited by Harsh Dadwal(731805) and Abhishek Parikhh (732182)
+ * GitHub Repository for images  https://github.com/harshdadwal/MCUX/tree/master/FreeRTOS%20Blinky%20IMG
+ * GitHub Repository for code    https://github.com/harshdadwal/MCUX/blob/master/freertos_blinky/example/src/freertos_blinky.c
+ *
+ *
+ *
  */
 
 #include "board.h"
